40bbc8ec1375f7a98952debb68c3213eca19c5f2
harshlsoni <harshlsoni2920@gmail.com>
2026-03-01 18:28:38 +0530

    dataset creation code

---
+++ Dataset_Creation/roboflow_upload.py
@@ -0,0 +1,33 @@
+import os
+from roboflow import Roboflow
+
+# Initialize Roboflow
+rf = Roboflow(api_key="xChTu2x7E9ULr1Vym0By")
+
+workspace_id = "abc-6ewki"
+project_id = "mealawe_annotation"
+
+project = rf.workspace(workspace_id).project(project_id)
+
+# Folder containing your images
+IMAGE_FOLDER = r"E:\\Mealawe-Kitchen-vendor-automation-project\\mydata\\meal_images"
+
+# Allowed image extensions
+VALID_EXTS = (".jpg", ".jpeg", ".png", ".bmp", ".webp")
+
+uploaded = 0
+
+for filename in os.listdir(IMAGE_FOLDER):
+    if filename.lower().endswith(VALID_EXTS):
+        image_path = os.path.join(IMAGE_FOLDER, filename)
+
+        print(f"Uploading → {filename}")
+
+        project.upload(
+            image_path=image_path, 
+            num_retry_uploads=3
+        )
+
+        uploaded += 1
+
+print(f"\n✅ Uploaded {uploaded} images successfully!")
